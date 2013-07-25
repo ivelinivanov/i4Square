95c34d2bde29ec6d29469cbd68b21f0c3fa16b66
Ivelin Ivanov <ivelin.ivanov@Student01s-Mac-mini.local>
2013-07-25 11:51:01 +0300

    Initial Commit

---
+++ i4Square/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  i4Square
+//
+//  Created by Ivelin Ivanov on 7/25/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ i4Square/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  i4Square
+//
+//  Created by Ivelin Ivanov on 7/25/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
